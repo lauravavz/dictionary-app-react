75e7a64d5007c6a2abc4e498e08616a6efe1c80e
Laura Vavz <vavzviewz@gmail.com>
2021-06-16 23:26:19 +0200

    Imported image logo

--- src/App.js
+++ src/App.js
@@ -1,9 +1,12 @@
 import './App.css';
+import logo from "./app-logo.png"
 
 function App() {
   return (
     <div className="App">
-      
+      <header className="App-header">
+     <img src={logo} className="App-logo" alt="app logo"/>
+      </header>
     </div>
   );
 }
