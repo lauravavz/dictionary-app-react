c7ee32beb646614418db301d2a2a98cd847e7b4a
Laura Vavz <vavzviewz@gmail.com>
2021-06-18 12:06:03 +0200

    Added form for search feature

--- src/App.js
+++ src/App.js
@@ -1,12 +1,18 @@
 import './App.css';
 import logo from "./app-logo.png"
+import Dictionary from "./Dictionary";
 
 function App() {
   return (
     <div className="App">
+      <div className="container">
       <header className="App-header">
      <img src={logo} className="App-logo" alt="app logo"/>
       </header>
+      <main>
+        <Dictionary />
+      </main>
+      </div>
     </div>
   );
 }
