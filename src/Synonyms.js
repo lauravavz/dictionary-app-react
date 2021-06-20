d71c3b0724386a4962f37901baecdfbfed9a6e05
Laura Vavz <vavzviewz@gmail.com>
2021-06-20 20:53:06 +0200

    Rendered synonyms

--- src/Synonyms.js
+++ src/Synonyms.js
@@ -1,5 +1,19 @@
 import React from "react";
+import "./Synonyms.css";
 
 export default function Synonyms(props) {
-    return "hello from synonyms";
+    if (props.synonyms) {
+    return (
+        <ul className="Synonyms">
+        {props.synonyms.map(function(synonym, index) {
+            return (
+                <li key={index}>
+                {synonym}
+                </li>);
+        })}
+        </ul>
+    );
+} else {
+        return null;
+    }
 }
