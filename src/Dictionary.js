84202fa6b5768a64fd3a16d83275ef5e3ba2e952
Laura Vavz <vavzviewz@gmail.com>
2021-06-20 11:39:10 +0200

    Added center text align to Dictionary.css

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -24,7 +24,7 @@ export default function Dictionary() {
 
     return(
         <div className="Dictionary">
-            <form onSubmit={Search}>
+            <form onSubmit={Search} className="Search-field">
                <input type="search" onChange={handleKeywordChange}/>  
             </form>
             <Results results={results} />
