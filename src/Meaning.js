6669bc94b3515e6bdba4c7009aa09344c2a6b333
Laura Vavz <vavzviewz@gmail.com>
2021-06-21 18:59:30 +0200

    CSS styling for index and dictionary components

--- src/Meaning.js
+++ src/Meaning.js
@@ -1,5 +1,4 @@
 import React from "react";
-import Synonyms from "./Synonyms";
 
 export default function Meaning(props) {
     return (
@@ -12,8 +11,6 @@ export default function Meaning(props) {
                     <strong>Definition: </strong>{definition.definition}
                     <br />
                     <strong>Example: </strong><em>{definition.example}</em>
-                    <br />
-                    <Synonyms synonyms={definition.synonyms} />
                     </p>
                 </div>
             );
