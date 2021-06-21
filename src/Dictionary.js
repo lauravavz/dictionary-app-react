554bd82608d6745348d4b0462c7be0bca7920b0a
Laura Vavz <vavzviewz@gmail.com>
2021-06-21 21:06:41 +0200

    Modified Photos component and added Photos CSS

--- src/Dictionary.js
+++ src/Dictionary.js
@@ -23,7 +23,7 @@ export default function Dictionary() {
         axios.get(apiUrl).then(handleDictionaryResponse);
 
         let pexelsApiKey = "563492ad6f917000010000016ae5ee198d504fc491478a0fefda2c43";
-        let pexelsApiUrl =  `https://api.pexels.com/v1/search?query=${keyword}&per_page=1`;
+        let pexelsApiUrl =  `https://api.pexels.com/v1/search?query=${keyword}&per_page=9`;
         let headers = { Authorization: `Bearer ${pexelsApiKey}`}
         axios.get(pexelsApiUrl, { headers: headers }).then(handlePexelsResponse);
     }
--- src/Photos.js
+++ src/Photos.js
@@ -1,4 +1,5 @@
 import React from "react";
+import "./Photos.css";
 
 export default function Photos(props) {
     if (props.photos) {
@@ -8,7 +9,9 @@ export default function Photos(props) {
             {props.photos.map(function(photo, index){
                 return (
                     <div className="col-4" key={index}>
+                        <a href={photo.src.original} target="_blank" rel="noreferrer">
                     <img src={photo.src.landscape} alt="word" className="img-fluid"/>
+                    </a>
                     </div>
                 );
             })}
